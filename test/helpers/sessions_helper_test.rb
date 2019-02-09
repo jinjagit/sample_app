143331941a539c024f9e71705a6d214b69e309ef
Simon Tharby <jinjamail@gmail.com>
2019-02-09 16:27:40 +0000

    Add account activation

--- test/helpers/sessions_helper_test.rb
+++ test/helpers/sessions_helper_test.rb
@@ -7,12 +7,10 @@ class SessionsHelperTest < ActionView::TestCase
     remember(@user)
   end
 
-=begin # I cannot get this test to pass, and I do not know why!
   test "current_user returns right user when session is nil" do
     assert_equal @user, current_user
     assert current_user?(@user)
   end
-=end
 
   test "current_user returns nil when remember digest is wrong" do
     @user.update_attribute(:remember_digest, User.digest(User.new_token))
--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -4,6 +4,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
     @admin     = users(:michael)
     @non_admin = users(:archer)
+    @lana      = users(:lana)
   end
 
   test "index as admin including pagination and delete links" do
@@ -11,11 +12,17 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination'
+    # check non-activated users not index;
+    # from: https://stackoverflow.com/questions/32211247/railstutorial-integration-tests-for-activated-users
     first_page_of_users = User.paginate(page: 1)
     first_page_of_users.each do |user|
-      assert_select 'a[href=?]', user_path(user), text: user.name
-      unless user == @admin
-        assert_select 'a[href=?]', user_path(user), text: 'delete'
+      if user.activated?
+        assert_select 'a[href=?]', user_path(user), text: user.name
+        unless user == @admin
+          assert_select 'a[href=?]', user_path(user), text: 'delete'
+        end
+      else
+        assert_select 'a[href=?]', user_path(user), text: user.name, count: 0
       end
     end
     assert_difference 'User.count', -1 do
